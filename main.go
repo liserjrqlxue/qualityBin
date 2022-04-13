d6e3019d99dc67abfcb11321db91d15c3564b840
Wang Yaoshen <wangyaoshen@bgi.com>
2022-04-13 14:34:45 +0800

    feat::sparkles:-inList for multi fastq file input

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bufio"
 	"flag"
+	"io"
+	"os"
+	"path/filepath"
 	"regexp"
-
 	//"compress/gzip"
 	gzip "github.com/klauspost/pgzip"
+	"github.com/liserjrqlxue/goUtil/textUtil"
 
 	"github.com/liserjrqlxue/goUtil/osUtil"
 	"github.com/liserjrqlxue/goUtil/simpleUtil"
@@ -15,7 +18,8 @@ import (
 // flag
 var (
 	in     = flag.String("in", "", "input file")
-	out    = flag.String("out", "", "output file")
+	inList = flag.String("inList", "", "input file list")
+	outDir = flag.String("out", "", "output directory")
 	offset = flag.Int("offset", 33, "quality offset")
 )
 
@@ -24,15 +28,20 @@ var (
 	isGz = regexp.MustCompile(`\.gz$`)
 )
 
+var (
+	qualityBins map[byte]byte
+)
+
 func main() {
 	flag.Parse()
-	if *in == "" || *out == "" {
-		flag.PrintDefaults()
+	if *in == "" && *inList == "" || *outDir == "" {
+		flag.Usage()
 		return
 	}
 
+	// init
 	// quality to bins
-	var qualityBins = make(map[byte]byte)
+	qualityBins = make(map[byte]byte)
 	var bins = [9]int{0, 0, 3, 11, 20, 23, 30, 37, 100}
 	for i, bin := range bins {
 		bins[i] = bin + *offset
@@ -43,28 +52,45 @@ func main() {
 		}
 	}
 
-	// read file
-	var input = osUtil.Open(*in)
-	defer simpleUtil.DeferClose(input)
-	var scanner *bufio.Scanner
-	if isGz.MatchString(*in) {
-		var gr = simpleUtil.HandleError(gzip.NewReader(input)).(*gzip.Reader)
-		scanner = bufio.NewScanner(gr)
+	// prepare input
+	var (
+		inputs []string
+	)
+	if *in != "" {
+		inputs = append(inputs, *in)
+	}
+	if *inList != "" {
+		for _, s := range textUtil.File2Array(*inList) {
+			inputs = append(inputs, s)
+		}
+	}
+
+	simpleUtil.CheckErr(os.MkdirAll(*outDir, 0755))
+
+	for _, input := range inputs {
+		var output = filepath.Join(*outDir, filepath.Base(input))
+		qualityBin(input, output)
+	}
+}
+
+func qualityBin(input, output string) {
+	var inFile = osUtil.Open(input)
+	var outFile = osUtil.Create(output)
+	if isGz.MatchString(input) {
+		var gr = simpleUtil.HandleError(gzip.NewReader(inFile)).(*gzip.Reader)
+		var gw = gzip.NewWriter(outFile)
+		quality2bin(gr, gw)
+		simpleUtil.CheckErr(inFile.Close())
+		simpleUtil.CheckErr(outFile.Close())
 	} else {
-		scanner = bufio.NewScanner(input)
+		quality2bin(inFile, outFile)
 	}
 
-	// write file
-	var output = osUtil.Create(*out)
-	defer simpleUtil.DeferClose(output)
-	var outZw *gzip.Writer
-	if isGz.MatchString(*out) {
-		outZw = gzip.NewWriter(output)
-		defer simpleUtil.DeferClose(outZw)
-	}
+}
 
-	// write
+func quality2bin(input io.ReadCloser, output io.WriteCloser) {
 	var n = 0
+	var scanner = bufio.NewScanner(input)
 	for scanner.Scan() {
 		var line = scanner.Bytes()
 		n++
@@ -73,10 +99,9 @@ func main() {
 				line[i] = qualityBins[b]
 			}
 		}
-		if outZw != nil {
-			simpleUtil.HandleError(outZw.Write(append(line, '\n')))
-		} else {
-			simpleUtil.HandleError(outZw.Write(append(line, '\n')))
-		}
+		simpleUtil.HandleError(output.Write(append(line, '\n')))
 	}
+	simpleUtil.CheckErr(scanner.Err())
+	simpleUtil.CheckErr(input.Close())
+	simpleUtil.CheckErr(output.Close())
 }
